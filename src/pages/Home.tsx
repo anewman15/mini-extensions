b8c0b26172adee1fdb041da278e2bef5ad8b455a
Abdullah Numan <anewman15@hotmail.com>
2021-09-28 23:24:46 +0600

    Create Home comp

---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Login from './Login';
+import UserPage from './UserPage';
+
+const Home = () => {
+  return (
+    <>
+      <Login />
+      <UserPage />
+    </>
+  );
+};
+
+export default Home;
