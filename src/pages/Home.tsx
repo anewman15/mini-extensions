1ef4d0b7f0542f5f5325a8391f1e7d522e83715c
Abdullah Numan <anewman15@hotmail.com>
2021-09-29 21:18:25 +0600

    Connect Home comp to store, add student name, ClassCards

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
-import Login from './Login';
-import UserPage from './UserPage';
+import { connect } from 'react-redux';
+import ClassCard from '../components/ClassCard';
 
-const Home = () => {
+const Home = ({ user, classes, students}: any) => {
   return (
-    <>
-      <Login />
-      <UserPage />
-    </>
+    <div className="container mx-auto my-6 p-4 w-10/12 md:w-8/12">
+      <h2 className="text-green-800 text-center text-4xl font-extrabold">{ user.fields.Name }</h2>
+      <div>
+        { classes.map((cl: any) => (
+          <ClassCard cl={cl} students={students} />
+        ))}
+      </div>
+    </div>
   );
 };
 
-export default Home;
+const mapStateToProps = (state: any) => ({
+  user: state.user,
+  classes: state.classes,
+  students: state.students
+});
+
+export default connect(mapStateToProps)(Home);
