5ca96b612a32ea671edb3422e0420ae4ac9d843d
Abdullah Numan <anewman15@hotmail.com>
2021-09-29 19:26:42 +0600

    Provide store to App comp

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import store from './redux/store/store';
 import Routes from './routes/routes';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
+    </Provider>
   );
 };
 
