90554025099eeb196ed4abb6fac1f3fe2c57723d
Abdullah Numan <anewman15@hotmail.com>
2021-09-30 20:11:48 +0600

    Remove comment from filterString.ts

--- src/utils/filterString.ts
+++ src/utils/filterString.ts
@@ -1,3 +1,5 @@
+// import { Record, FieldSet } from "airtable";
+
 const filterString = (ids: string[]) => {
   const string_arr = ids.map((id: string) => `RECORD_ID() = '${id}'`);
   return string_arr.join(', ');
