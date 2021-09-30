cdc38eb79dd3640890215c542e12d0b56a2306a3
Abdullah Numan <anewman15@hotmail.com>
2021-09-30 20:50:50 +0600

    Add type for user action

--- src/redux/reducers/user.ts
+++ src/redux/reducers/user.ts
@@ -1,5 +1,11 @@
+type UserActionType = {
+  type: 'SAVE_USER',
+  payload: {
+    userData: any
+  }
+};
 
-const user = (state = {}, action: any) => {
+const user = (state = {}, action: UserActionType) => {
   switch (action.type) {
     case 'SAVE_USER': {
       return action.payload.userData
