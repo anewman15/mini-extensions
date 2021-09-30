43ceda524ebd18a8776bd5588bf5749838f40552
Abdullah Numan <anewman15@hotmail.com>
2021-09-30 20:12:07 +0600

    Adjust import statement for store

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import store from './redux/store/store';
+import { store } from './redux/store/store';
 import Routes from './routes/routes';
 
 function App() {
