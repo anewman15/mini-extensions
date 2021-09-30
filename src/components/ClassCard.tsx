b16a1cb0f3f83edb226b639772f21faad2fa531c
Abdullah Numan <anewman15@hotmail.com>
2021-09-30 20:10:47 +0600

    Add types for ClassCard comp

--- src/components/ClassCard.tsx
+++ src/components/ClassCard.tsx
@@ -1,6 +1,13 @@
+import { Record, FieldSet } from 'airtable';
 import React from 'react'
+type ClassCardProps = {
+  cl: Record<FieldSet> | any,
+  students: {
+    [key:string]: string,
+  }
+};
 
-const ClassCard = ({ cl, students }: any) => {
+const ClassCard = ({ cl, students }: ClassCardProps) => {
   return (
     <div key={cl.id} className="mx-4 my-12 shadow-lg border rounded-b-md">
       <h2 className="px-4 py-1 bg-green-600 text-2xl text-center text-gray-100 font-extrabold">{ cl.fields.Name }</h2>
