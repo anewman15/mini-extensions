65ead5dc2edfeebe545acf3ea78a949f450c1a34
Abdullah Numan <anewman15@hotmail.com>
2021-09-30 14:36:00 +0600

    Use localStorage data for store initial state

--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -1,41 +1,21 @@
+import { Record, Records, FieldSet } from 'airtable';
 import { createStore } from 'redux';
 import combinedReducer from '../reducers';
+import { loadStoreFromLocalStorage, saveStoreToLocaStorage } from './persist';
 
-// type initialStateType = {
-//   user: Record<FieldSet>,
-//   classes: Records<FieldSet>,
-//   students: {
-//     [key: string]: string
-//   }
-// };
-
-const initialState: any = {
-  user: {
-    id: '',
-    fields: {
-      Name: '',
-      Classes: [
-        '', ''
-      ],
-    }
-  },
-  classes: [
-    {
-      id: '',
-      fields: {
-        Name: '',
-        Students: [],
-      }
-    }
-  ],
-  students: {
-    "": "",
-  }
+type initialStateType = {
+  user: Record<FieldSet> | null,
+  classes: Records<FieldSet> | null,
+  students: undefined
 };
+
+const initialState: initialStateType = loadStoreFromLocalStorage();
 
 const store = createStore(
   combinedReducer,
   initialState,
 );
+
+store.subscribe(() => saveStoreToLocaStorage(store.getState()));
 
 export default store;
