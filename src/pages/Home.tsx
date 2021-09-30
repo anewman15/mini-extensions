62acd55f97e7e76121f345c1717478d5ed306552
Abdullah Numan <anewman15@hotmail.com>
2021-09-30 14:39:21 +0600

    Add Log out button to Home comp

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,32 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import ClassCard from '../components/ClassCard';
+import saveUser from '../redux/actions/saveUser';
+import saveClasses from '../redux/actions/saveClasses';
+import saveStudents from '../redux/actions/saveStudents';
 
-const Home = ({ user, classes, students}: any) => {
+const Home = ({ user, saveUser, classes, saveClasses, students, saveStudents }: any) => {
+  const logOut = () => {
+    saveUser({});
+    saveClasses([]);
+    saveStudents({});
+  }
+
   return (
-    <div className="container mx-auto my-6 p-4 w-10/12 md:w-8/12">
-      <h2 className="text-green-800 text-center text-4xl font-extrabold">{ user.fields.Name }</h2>
+    <div className="relative container mx-auto my-6 p-4 w-10/12 md:w-8/12">
+      <div className="absolute top-0 right-8">
+        <button
+          className="bg-red-400 my-2 px-3 py-1 rounded-md text-gray-100"
+          type="button"
+          onClick={logOut}
+        >
+          Log out
+        </button>
+      </div>
+      <h2 className="text-green-800 text-center text-4xl font-extrabold">{ user.fields && user.fields.Name }</h2>
       <div>
-        { classes.map((cl: any) => (
-          <ClassCard cl={cl} students={students} />
+        { classes.length && classes.map((cl: any) => (
+          <ClassCard key={cl.id} cl={cl} students={students} />
         ))}
       </div>
     </div>
@@ -21,4 +39,4 @@ const mapStateToProps = (state: any) => ({
   students: state.students
 });
 
-export default connect(mapStateToProps)(Home);
+export default connect(mapStateToProps, { saveUser, saveClasses, saveStudents })(Home);
