b4d586cadf47d8344d4484070fc6dac3caf7eb1e
Abdullah Numan <anewman15@hotmail.com>
2021-09-30 14:44:11 +0600

    Add shadow, margin to ClassCard comp

--- src/components/ClassCard.tsx
+++ src/components/ClassCard.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ClassCard = ({ cl, students }: any) => {
   return (
-    <div key={cl.id} className="mx-4 my-6 border rounded-b-md">
+    <div key={cl.id} className="mx-4 my-12 shadow-lg border rounded-b-md">
       <h2 className="px-4 py-1 bg-green-600 text-2xl text-center text-gray-100 font-extrabold">{ cl.fields.Name }</h2>
       <hr />
       <div className="px-4 py-6 text-center flex flex-wrap justify-center items-center">
