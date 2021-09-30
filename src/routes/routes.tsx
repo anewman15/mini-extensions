cd64bbabc5bc8b89e536d1a72e093e9de5b1478a
Abdullah Numan <anewman15@hotmail.com>
2021-09-30 14:39:58 +0600

    Use AuthenticatedRoute and AuthenticationRoute

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Switch, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import Login from '../pages/Login';
+import { AuthenticatedRoute, AuthenticationRoute } from './authentication';
 
-const Routes = () => {
+const Routes = ({ user }: any) => {
   return (
   <Switch>
-    <Route exact path="/" component={Home}></Route>
-    <Route exact path="/login" component={Login}></Route>
+    <AuthenticatedRoute exact path="/" user={user} component={Home}></AuthenticatedRoute>
+    <AuthenticationRoute exact path="/login" user={user} component={Login}></AuthenticationRoute>
   </Switch>
   );
 };
 
-export default Routes;
+const mapStateToProps = (state: any) => ({
+  user: state.user,
+})
+
+export default connect(mapStateToProps)(Routes);
